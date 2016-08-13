fc480052ddce774187a16781b696c94ab461ea77
TJ Mahr <mahr@wisc.edu>
2016-08-13 09:57:23 -0500

    run through chapter 2

--- 02-Exploration/chapter02.R
+++ 02-Exploration/chapter02.R
@@ -1,5 +1,5 @@
-# File-Name:       chapter02.R           
-# Date:            2011-11-11                                
+# File-Name:       chapter02.R
+# Date:            2011-11-11
 # Author:          John Myles White
 # Email:           jmw@johnmyleswhite.com
 # Purpose:         Code for Chapter 2. Showcases tools for exploratory data analysis.
@@ -7,10 +7,10 @@
 # Packages Used:   ggplot2
 # Machine:         John Myles White's MacBook
 
-# All source code is copyright (c) 2011, under the Simplified BSD License.  
+# All source code is copyright (c) 2011, under the Simplified BSD License.
 # For more information on FreeBSD see: http://www.opensource.org/licenses/bsd-license.php
 
-# All images and materials produced by this code are licensed under the Creative Commons 
+# All images and materials produced by this code are licensed under the Creative Commons
 # Attribution-Share Alike 3.0 United States License: http://creativecommons.org/licenses/by-sa/3.0/us/
 
 # All rights reserved.
@@ -20,39 +20,35 @@
 #
 
 # Load in the data set from disk.
-data.file <- file.path('data', '01_heights_weights_genders.csv')
-heights.weights <- read.csv(data.file, header = TRUE, sep = ',')
+data_file <- file.path(".", "02-Exploration", "data",
+                       "01_heights_weights_genders.csv")
+heights_weights <- readr::read_csv(data_file)
 
 # Create a numeric vector containing just the heights data.
-heights <- with(heights.weights, Height)
+heights <- with(heights_weights, Height)
 summary(heights)
 
 # Expected output.
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-#54.26   63.51   66.32   66.37   69.17   79.00 
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+#54.26   63.51   66.32   66.37   69.17   79.00
 
 #
 # Snippet 2
 #
 
 # Define our own mean and median functions.
-my.mean <- function(x)
-{
+my_mean <- function(x) {
   return(sum(x) / length(x))
 }
 
-my.median <- function(x)
-{
-  sorted.x <- sort(x)
-  if (length(x) %% 2 == 0)
-  {
+my_median <- function(x) {
+  sorted_x <- sort(x)
+  if (length(x) %% 2 == 0) {
     indices <- c(length(x) / 2, length(x) / 2 + 1)
-    return(mean(sorted.x[indices]))
-  }
-  else
-  {
+    return(mean(sorted_x[indices]))
+  } else {
     index <- ceiling(length(x) / 2)
-    return(sorted.x[index])
+    return(sorted_x[index])
   }
 }
 
@@ -61,23 +57,23 @@ my.median <- function(x)
 #
 
 # Compare means and medians on toy examples.
-my.vector <- c(0, 100)
+my_vector <- c(0, 100)
 
-my.vector
+my_vector
 # [1]	0 100
 
-mean(my.vector)
+mean(my_vector)
 #[1] 50
 
-median(my.vector)
+median(my_vector)
 #[1] 50
 
-my.vector <- c(0, 0, 100)
+my_vector <- c(0, 0, 100)
 
-mean(my.vector)
+mean(my_vector)
 #[1] 33.33333
 
-median(my.vector)
+median(my_vector)
 #[1] 0
 
 #
@@ -85,16 +81,16 @@ median(my.vector)
 #
 
 # Confirm that our mean and median functions produce the correct answer.
-my.mean(heights)
+my_mean(heights)
 #[1] 66.36756
 
-my.median(heights)
+my_median(heights)
 #[1] 66.31807
 
-mean(heights) - my.mean(heights)
+mean(heights) - my_mean(heights)
 #[1] 0
 
-median(heights) - my.median(heights)
+median(heights) - my_median(heights)
 #[1] 0
 
 #
@@ -128,16 +124,16 @@ range(heights)
 
 # Try out the quantile function for computing arbitrary quantiles.
 quantile(heights)
-#      0%      25%      50%      75%     100% 
-#54.26313 63.50562 66.31807 69.17426 78.99874 
+#      0%      25%      50%      75%     100%
+#54.26313 63.50562 66.31807 69.17426 78.99874
 
 #
 # Snippet 9
 #
 
 quantile(heights, probs = seq(0, 1, by = 0.20))
-#      0%      20%      40%      60%      80%     100% 
-#54.26313 62.85901 65.19422 67.43537 69.81162 78.99874 
+#      0%      20%      40%      60%      80%     100%
+#54.26313 62.85901 65.19422 67.43537 69.81162 78.99874
 
 #
 # Snippet 10
@@ -151,10 +147,9 @@ seq(0, 1, by = 0.20)
 #
 
 # Define a variance function to assess the spread of data.
-my.var <- function(x)
-{
+my_var <- function(x) {
   m <- mean(x)
-  return(sum((x - m) ^ 2) / length(x))
+  sum((x - m) ^ 2) / length(x)
 }
 
 #
@@ -162,21 +157,20 @@ my.var <- function(x)
 #
 
 # Test our variance function for correctness.
-my.var(heights) - var(heights)
+my_var(heights) - var(heights)
 
 #
 # Snippet 13
 #
 
 # Update the variance function to make it unbiased.
-my.var <- function(x)
-{
+my_var <- function(x) {
   m <- mean(x)
-  return(sum((x - m) ^ 2) / (length(x) - 1))
+  sum((x - m) ^ 2) / (length(x) - 1)
 }
 
 # Test our variance function again for correctness.
-my.var(heights) - var(heights)
+my_var(heights) - var(heights)
 
 #
 # Snippet 14
@@ -200,9 +194,8 @@ range(heights)
 #
 
 # Switch to standard deviations instead for thinking about ranges.
-my.sd <- function(x)
-{
-  return(sqrt(my.var(x)))
+my_sd <- function(x) {
+  return(sqrt(my_var(x)))
 }
 
 #
@@ -210,7 +203,7 @@ my.sd <- function(x)
 #
 
 # Test our standard deviation function for correctness.
-my.sd(heights) - sd(heights)
+my_sd(heights) - sd(heights)
 
 #
 # Snippet 18
@@ -230,36 +223,35 @@ c(mean(heights) - sd(heights), mean(heights) + sd(heights))
 # [1] 62.52003 70.21509
 
 c(quantile(heights, probs = 0.25), quantile(heights, probs = 0.75))
-#     25%      75% 
-#63.50562 69.17426 
+#     25%      75%
+#63.50562 69.17426
 
 #
 # Snippet 20
 #
 
 # Start visualizing data using the ggplot2 package.
-library('ggplot2')
+library("ggplot2")
 
 # Load the data from scratch for purity.
-data.file <- file.path('data', '01_heights_weights_genders.csv')
-heights.weights <- read.csv(data.file, header = TRUE, sep = ',')
+heights_weights <- read.csv(data_file, header = TRUE, sep = ",")
 
 # Experiment with histograms.
-ggplot(heights.weights, aes(x = Height)) +
+ggplot(heights_weights, aes(x = Height)) +
   geom_histogram(binwidth = 1)
 
 #
 # Snippet 21
 #
 
-ggplot(heights.weights, aes(x = Height)) +
+ggplot(heights_weights, aes(x = Height)) +
   geom_histogram(binwidth = 5)
 
 #
 # Snippet 22
 #
 
-ggplot(heights.weights, aes(x = Height)) +
+ggplot(heights_weights, aes(x = Height)) +
   geom_histogram(binwidth = 0.01)
 
 #
@@ -267,7 +259,7 @@ ggplot(heights.weights, aes(x = Height)) +
 #
 
 # Experiment with kernel density estimates.
-ggplot(heights.weights, aes(x = Height)) +
+ggplot(heights_weights, aes(x = Height)) +
   geom_density()
 
 #
@@ -275,14 +267,14 @@ ggplot(heights.weights, aes(x = Height)) +
 #
 
 # Separate out heights and weights based on gender.
-ggplot(heights.weights, aes(x = Height, fill = Gender)) +
+ggplot(heights_weights, aes(x = Height, fill = Gender)) +
   geom_density()
 
 #
 # Snippet 25
 #
 
-ggplot(heights.weights, aes(x = Weight, fill = Gender)) +
+ggplot(heights_weights, aes(x = Weight, fill = Gender)) +
   geom_density()
 
 #
@@ -290,7 +282,7 @@ ggplot(heights.weights, aes(x = Weight, fill = Gender)) +
 #
 
 # Produce two facets in a single plot to make it easier to see the hidden structure.
-ggplot(heights.weights, aes(x = Weight, fill = Gender)) +
+ggplot(heights_weights, aes(x = Weight, fill = Gender)) +
   geom_density() +
   facet_grid(Gender ~ .)
 
@@ -310,18 +302,18 @@ ggplot(data.frame(X = rnorm(100000, m, s)), aes(x = X)) +
 
 # Compare the normal distribution with the Cauchy distribution.
 set.seed(1)
-normal.values <- rnorm(250, 0, 1)
-cauchy.values <- rcauchy(250, 0, 1)
-range(normal.values)
-range(cauchy.values)
+normal_values <- rnorm(250, 0, 1)
+cauchy_values <- rcauchy(250, 0, 1)
+range(normal_values)
+range(cauchy_values)
 
 #
 # Snippet 29
 #
 
-ggplot(data.frame(X = normal.values), aes(x = X)) +
+ggplot(data.frame(X = normal_values), aes(x = X)) +
   geom_density()
-ggplot(data.frame(X = cauchy.values), aes(x = X)) +
+ggplot(data.frame(X = cauchy_values), aes(x = X)) +
   geom_density()
 
 #
@@ -329,8 +321,8 @@ ggplot(data.frame(X = cauchy.values), aes(x = X)) +
 #
 
 # Experiment with random numbers from the gamma distribution.
-gamma.values <- rgamma(100000, 1, 0.001)
-ggplot(data.frame(X = gamma.values), aes(x = X)) +
+gamma_values <- rgamma(100000, 1, 0.001)
+ggplot(data.frame(X = gamma_values), aes(x = X)) +
   geom_density()
 
 #
@@ -338,7 +330,7 @@ ggplot(data.frame(X = gamma.values), aes(x = X)) +
 #
 
 # Generate scatterplots of the heights and weights to see their relationship.
-ggplot(heights.weights, aes(x = Height, y = Weight)) +
+ggplot(heights_weights, aes(x = Height, y = Weight)) +
   geom_point()
 
 #
@@ -346,7 +338,7 @@ ggplot(heights.weights, aes(x = Height, y = Weight)) +
 #
 
 # Add a smooth shape that relates the two explicitly.
-ggplot(heights.weights, aes(x = Height, y = Weight)) +
+ggplot(heights_weights, aes(x = Height, y = Weight)) +
   geom_point() +
   geom_smooth()
 
@@ -355,48 +347,44 @@ ggplot(heights.weights, aes(x = Height, y = Weight)) +
 #
 
 # See how the smooth shape gets better with more data.
-ggplot(heights.weights[1:20, ], aes(x = Height, y = Weight)) +
+ggplot(heights_weights[1:20, ], aes(x = Height, y = Weight)) +
   geom_point() +
   geom_smooth()
-ggplot(heights.weights[1:200, ], aes(x = Height, y = Weight)) +
-  geom_point() +
-  geom_smooth()
-ggplot(heights.weights[1:2000, ], aes(x = Height, y = Weight)) +
-  geom_point() +
-  geom_smooth()
+
+last_plot() %+% heights_weights[1:200, ]
+last_plot() %+% heights_weights[1:2000, ]
 
 #
 # Snippet 34
 #
 
 # Visualize how gender depends on height and weight.
-ggplot(heights.weights, aes(x = Height, y = Weight)) +
+ggplot(heights_weights, aes(x = Height, y = Weight)) +
   geom_point(aes(color = Gender, alpha = 0.25)) +
-  scale_alpha(guide = "none") + 
+  scale_alpha(guide = "none") +
   scale_color_manual(values = c("Male" = "black", "Female" = "gray")) +
   theme_bw()
 
 # An alternative using bright colors.
-ggplot(heights.weights, aes(x = Height, y = Weight, color = Gender)) +
+ggplot(heights_weights, aes(x = Height, y = Weight, color = Gender)) +
   geom_point()
 
 #
 # Snippet 35
 #
 
-heights.weights <- transform(heights.weights,
-                             Male = ifelse(Gender == 'Male', 1, 0))
+heights_weights <- transform(heights_weights,
+                             Male = ifelse(Gender == "Male", 1, 0))
 
-logit.model <- glm(Male ~ Weight + Height,
-                   data = heights.weights,
-                   family = binomial(link = 'logit'))
+logit_model <- glm(Male ~ Weight + Height,
+                   data = heights_weights,
+                   family = binomial(link = "logit"))
 
-ggplot(heights.weights, aes(x = Height, y = Weight)) +
+ggplot(heights_weights, aes(x = Height, y = Weight)) +
   geom_point(aes(color = Gender, alpha = 0.25)) +
-  scale_alpha(guide = "none") + 
+  scale_alpha(guide = "none") +
   scale_color_manual(values = c("Male" = "black", "Female" = "gray")) +
   theme_bw() +
-  stat_abline(intercept = -coef(logit.model)[1] / coef(logit.model)[2],
-              slope = - coef(logit.model)[3] / coef(logit.model)[2],
-              geom = 'abline',
-              color = 'black')
+  geom_abline(intercept = -coef(logit_model)[1] / coef(logit_model)[2],
+              slope = -coef(logit_model)[3] / coef(logit_model)[2],
+              color = "black")
